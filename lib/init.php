2bd3ef0b0329ef55b9a6ed47d78b28a12e749dfb
sandro3 <sandro3@gmail.com>
2014-06-09 13:14:31 +1000

    Fixed unix paths

--- lib/init.php
+++ lib/init.php
@@ -3,7 +3,8 @@
 define('BASE_PATH', realpath(dirname(__FILE__)));
 
 function __autoload($className) {
-    $temp = BASE_PATH . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $className . '.php';
+	$temp = str_replace('\\', '/', $className);
+    $temp = BASE_PATH . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $temp . '.php';
     if (file_exists($temp)) {
         require_once $temp;
         return true;
